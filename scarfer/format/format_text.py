887c48e08535a3a63b5c1446e1676f26ca8adbe7
Sandklef Henrik <hesa@sandklef.com>
2023-01-24 12:56:44 +0100

    restructure, add new format

--- scarfer/format/format_text.py
+++ scarfer/format/format_text.py
@@ -1,4 +1,5 @@
 from scarfer.format.interface import FormatInterface
+import os
 
 class TextFormatter(FormatInterface):
 
@@ -30,42 +31,45 @@ class TextFormatter(FormatInterface):
             ret.append(self._format_file(f, settings))
         return "\n".join(ret)
 
-    def _format_cumulative(self, report, settings):
+    def format(self, report, settings):
+        results = [
+            "Files:\n----------------------------",
+            self._format_files(report, settings),
+        ]
+        return "\n".join(results)
+
+    def format_fixes(self, fixes, settings={}):
+        excluded = [ 'Filtered out:' ]
+        for f in fixes['excluded_files']:
+            excluded.append(f' * {f["path"]}')
+        missing = ['Missing license fixed:'] 
+        for curation in fixes['missing_license']:
+            missing.append(f' * {curation["file"]} -> {curation["curation"]}')
+        return f'{os.linesep.join(excluded)}{os.linesep}{os.linesep.join(missing)}'
+
+    def format_cumulative(self, report, settings={}):
         ret = []
         cumulative = report['cumulative']
-        ret.append(" * license: {_license}".format(_license=cumulative['license']))
+        ret.append(f"Cumulative license: {cumulative['license']}")
         return "\n".join(ret)
         
-    
-    def format(self, report, settings):
+    def format_license_summary(self, report, settings={}):
+        license_summary = set()
+        for f in report['files']:
+            for l in f['license']['expressions']:
+                license_summary.add(l)
+        return f'License:\n { " AND ".join(license_summary)}\n'
 
-        if settings.get('license_summary') or settings.get('copyright_summary'):
-            res = ""
-            if settings.get('license_summary'):
-                license_summary = set()
-                for f in report['files']:
-                    for l in f['license']['expressions']:
-                        license_summary.add(l)
-                res += f'Licenses:\n { " AND ".join(license_summary)}\n'
+    def format_copyright_summary(self, report, settings={}):
+        copyright_summary = set()
+        for f in report['files']:
+            for l in f['copyrights']:
+                copyright_summary.add(l)
+        c_list = list(copyright_summary)
+        c_list.sort()
+        c_string = "\n".join(c_list)
+        return f'Copyrights:\n{c_string}\n'
 
-            if settings.get('copyright_summary'):
-                copyright_summary = set()
-                for f in report['files']:
-                    for l in f['copyrights']:
-                        copyright_summary.add(l)
-                c_list = list(copyright_summary)
-                c_list.sort()
-                c_string = "\n".join(c_list)
-                res += f'Copyrights:\n{c_string}\n'
-            return res
-        elif settings.get('cumulative'):
-            return self._format_cumulative(report, settings)
-        else:
-            results = [
-                "Files:\n----------------------------",
-                self._format_files(report, settings),
-            ]
-            return "\n".join(results)
 
 
         
