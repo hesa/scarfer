1d6bd58ae2b8100958f623b6814b91801b6fd42f
Sandklef Henrik <hesa@sandklef.com>
2023-01-24 12:56:17 +0100

    structure JSON output format

--- scarfer/format/format_json.py
+++ scarfer/format/format_json.py
@@ -5,5 +5,32 @@ from scarfer.format.interface import FormatInterface
 class JsonFormatter(FormatInterface):
 
     def format(self, report, settings={}):
-        return json.dumps(report, indent=4)
+        return json.dumps(report['files'], indent=4)
 
+    def format_cumulative(self, report, settings={}):
+        ret = []
+        cumulative = report['cumulative']
+        ret.append(" * license: {_license}".format(_license=cumulative['license']))
+        return "\n".join(ret)
+        
+    def format_license_summary(self, report, settings={}):
+        license_summary = set()
+        for f in report['files']:
+            for l in f['license']['expressions']:
+                license_summary.add(l)
+        return json.dumps({ "license": f'{ " AND ".join(license_summary)}' })
+
+    def format_copyright_summary(self, report, settings={}):
+        copyright_summary = set()
+        for f in report['files']:
+            for l in f['copyrights']:
+                copyright_summary.add(l)
+        c_list = list(copyright_summary)
+        c_list.sort()
+        c_string = "\n".join(c_list)
+        return json.dumps({ "copyrights": f'\n{c_string}' })
+
+
+
+        
+        
