dd52b4db8e71b5401587362e6af96c7746638642
Sandklef Henrik <hesa@sandklef.com>
2023-01-24 12:55:31 +0100

    new markdown formatter

--- scarfer/format/factory.py
+++ scarfer/format/factory.py
@@ -1,6 +1,7 @@
 from scarfer.format.format_json import JsonFormatter
 from scarfer.format.format_yaml import YamlFormatter
 from scarfer.format.format_text import TextFormatter
+from scarfer.format.format_markdown import MarkdownFormatter
 
 
 class FormatFactory:
@@ -13,3 +14,5 @@ class FormatFactory:
             return YamlFormatter()
         elif format.lower() == "text" or format.lower() == "txt":
             return TextFormatter()
+        elif format.lower() == "markdown" or format.lower() == "md":
+            return MarkdownFormatter()
