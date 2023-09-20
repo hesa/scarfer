2cc454776d2f1bf21699f037705a749a5f6a95c3
Henrik Sandklef <hesa@sandklef.com>
2023-09-20 22:29:16 +0200

    add (c) filter

--- scarfer/filter_utils.py
+++ scarfer/filter_utils.py
@@ -10,15 +10,21 @@ def _create_filter(expr, type):
 
 def _create_license_filter(expr):
     return _create_filter(expr, ScanReportFilterType.LICENSE)
-    
+
+def _create_copyright_filter(expr):
+    return _create_filter(expr, ScanReportFilterType.COPYRIGHT)
+
 def _create_file_filter(expr):
     return _create_filter(expr, ScanReportFilterType.FILE)
 
-def create_filters(license_filter, file_filter):
+def create_filters(license_filter, copyright_filter, file_filter):
     filters = []
     for lf in license_filter:
         filters.append(_create_license_filter(lf))
-        
+
+    for lf in copyright_filter:
+        filters.append(_create_copyright_filter(lf))
+
     for ff in file_filter:
         filters.append(_create_file_filter(ff))
     return filters
