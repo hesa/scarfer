3599ef073cb7a74cc49224e600b53d4206312a12
Henrik Sandklef <hesa@sandklef.com>
2023-10-19 22:58:58 +0200

    add regression test for license text matched

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import unittest
-from scarfer.format.factory import FormatFactory
 from scarfer.scan_interface import ScanReportReader
 from scarfer.analyzer import Analyzer
 
@@ -19,7 +18,7 @@ class TestScancodeReader(unittest.TestCase):
         """
         File cairo-1.16.0/util/show-events.c has no license
         make sure this is analyzed as  ['missing']
-        In 3.0.0, scancode says: []
+        In 2.0.0, scancode says: []
         """
         reader = ScanReportReader(CAIRO_REPORT_2)
         report = reader.read()
@@ -30,7 +29,7 @@ class TestScancodeReader(unittest.TestCase):
             if _file['path'] == 'cairo-1.16.0/util/show-events.c':
                 _file_lic = _file['license']['expressions']
                 self.assertEqual(_file_lic, ['missing'])
-        
+
     def test_missing_license_3_0_0(self):
         """
         File cairo-1.16.0/util/show-events.c has no license
@@ -46,5 +45,42 @@ class TestScancodeReader(unittest.TestCase):
             if _file['path'] == 'cairo-1.16.0/util/show-events.c':
                 _file_lic = _file['license']['expressions']
                 self.assertEqual(_file_lic, ['missing'])
-        
 
+    def test_matching_license_2_0_0(self):
+        """
+        File cairo-1.16.0/util/cairo-script/cairo-script-file.c has two licenses identified
+        """
+        reader = ScanReportReader(CAIRO_REPORT_2)
+        report = reader.read()
+        analyzer = Analyzer(report)
+        analyzer.apply_filters()
+        analyzed_report = analyzer.report()
+        for _file in analyzed_report['files']:
+            if _file['path'] == 'cairo-1.16.0/util/cairo-script/cairo-script-file.c':
+                match_count = len(_file['license']['matches'])
+                self.assertEqual(2, match_count)
+
+                file_lic = _file['license']['expressions']
+                self.assertEqual(['lgpl-2.1 OR mpl-1.1'], file_lic)
+
+    def test_matching_license_3_0_0(self):
+        """
+        File cairo-1.16.0/util/cairo-script/cairo-script-file.c has THREE licenses identified
+        """
+        reader = ScanReportReader(CAIRO_REPORT_3)
+        report = reader.read()
+        analyzer = Analyzer(report)
+        analyzer.apply_filters()
+        analyzed_report = analyzer.report()
+        for _file in analyzed_report['files']:
+            if _file['path'] == 'cairo-1.16.0/util/cairo-script/cairo-script-file.c':
+                match_count = len(_file['license']['matches'])
+                self.assertEqual(3, match_count)
+
+                file_lic = _file['license']['expressions']
+                # THIS EXPRESSION SHOULD BE WITH AN "AND" Until
+                # https://github.com/nexB/scancode-toolkit/issues/3523
+                # is fixed, we need to keep it this way When the
+                # scancode bug is fixed, rescan and replace current
+                # scan report
+                self.assertEqual(['lgpl-2.1 AND mpl-1.1'], file_lic)
